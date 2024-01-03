f54351f3cc3e0968646f0d1bd513c13365b51198
victortemple <templesambovictor@gmail.com>
2024-01-03 11:53:16 +0100

    cokies added

--- javascript_files/general.js
+++ javascript_files/general.js
@@ -15,3 +15,20 @@ changeBtnColor.style.color = '#000000'
 };
 
 
+const cookieBox = document.querySelector(".mainwrapper .wrapper"),
+
+acceptBtn = document.querySelector(".cookie-button .item");
+
+acceptBtn.onclick = ()=> {
+    document.cookie = "CookieBy=TempleVictorSambo; max-age="+60*60*24*30;
+
+    if(document.cookie){
+        cookieBox.classList.add("hide");
+    }
+    else{
+        alert("Cookie can't be set!");
+    }
+}
+
+let checkCookie = document.cookie.indexOf("CookieBy=TempleVictorSambo");
+checkCookie != -1 ?  cookieBox.classList.add("hide"):  cookieBox.classList.remove("hide");
